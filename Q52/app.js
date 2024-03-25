e4be6c49e1f21bdadf042821792158804a4cc285
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-26 01:47:53 +0500

    100dayscoding

---
+++ Q52/app.js
@@ -0,0 +1,15 @@
+// Question 52: Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+// Example smartphone object creation
+var myPhone = {
+    brand: "Apple",
+    model: "iPhone 14 Pro Max",
+    storage: 128,
+    screenSize: 6.7,
+    batteryLife: 28,
+};
+// Accessing phone details
+console.log("Brand:", myPhone.brand);
+console.log("Model:", myPhone.model);
+console.log("Storage:", myPhone.storage, "GB");
+console.log("Screen Size:", myPhone.screenSize, "inches");
+console.log("Battery Life:", myPhone.batteryLife, "hours (estimated)");

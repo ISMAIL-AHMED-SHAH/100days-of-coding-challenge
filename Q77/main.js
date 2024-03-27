21f2d4897872e4daec8e0b39bb137b17e21e3e96
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-27 06:11:36 +0500

    100DaysOfCoding...!

---
+++ Q77/main.js
@@ -0,0 +1,11 @@
+// Question 77: Default Parameters: Write a function that greets a user. It should take the user's name as a parameter and say hello. If no name is given, it should greet an anonymous user.
+// This function greets a user by name, or greets an anonymous user if no name is provided
+function greetUser(name) {
+    if (name === void 0) { name = "anonymous"; }
+    // Says hello to the given name or to an anonymous user
+    console.log("Hello, ".concat(name, "!"));
+}
+// Trying the function with a name and without
+greetUser("Ali"); // Outputs: Hello, Ali!
+greetUser(); // Outputs: Hello, anonymous!
+// We see how our function can say hello to someone by name, or just say hello if we don't know the name.

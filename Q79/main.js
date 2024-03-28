e608a0628f1190e534923687952c8cb32d09c64d
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-28 06:34:03 +0500

    100daysOfCoding

---
+++ Q79/main.js
@@ -0,0 +1,11 @@
+// Question 79: Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. Then, show how you can access the model property of the car.
+// This sets up an object for a car with specific details
+var car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2024
+};
+// Accessing and showing the car's model using dot notation
+console.log(car.model); // Outputs: Corolla
+// Accessing and showing the car's model using bracket notation
+console.log(car["model"]); // Outputs: Corolla

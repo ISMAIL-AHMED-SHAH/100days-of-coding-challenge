5e7199a9a62ace54e5499da860add40e791a8575
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-03-29 17:45:46 +0500

    100DaysOfCode

---
+++ Q82/main.js
@@ -0,0 +1,8 @@
+// Question 82: Find the Length of a String: Write a function that takes a string as an argument and returns the number of characters in the string.
+// This function counts how many characters are in a string
+function stringLength(str) {
+    return str.length; // Returns the number of characters in the string
+}
+// Example: Measuring the length of a name
+console.log(stringLength("ISMAIL AHMED SHAH")); // Outputs: 5
+// We're simply asking how long the string is, and it tells us there are 12 characters.
---
+++ Q82/main.ts
@@ -0,0 +1,13 @@
+// Question 82: Find the Length of a String: Write a function that takes a string as an argument and returns the number of characters in the string.
+
+// This function counts how many characters are in a string
+function stringLength(str: string): number {
+    return str.length; // Returns the number of characters in the string
+}
+
+// Example: Measuring the length of a name
+
+console.log(stringLength("ISMAIL AHMED SHAH")); // Outputs: 17
+// We're simply asking how long the string is, and it tells us there are 17 characters.
+
+

c170433daec2fa3bbb9e7938ae50805fc76b4945
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-02 07:00:09 +0500

    100DaysOfCode

---
+++ Q91/main.ts
@@ -0,0 +1,11 @@
+// Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
+
+// Create an array of favorite fruits
+let favoriteFruits1 = ['apple', 'banana', 'orange'];
+
+// Add a new fruit to the end of the array
+favoriteFruits1.push('strawberry');
+
+// Log the updated array
+console.log(favoriteFruits1);
+// This line adds a new fruit to our list of favorites.

64b2e8e84ef4f5a2d101e20b58aa9747466e19d8
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-04 07:13:47 +0500

    100DAYSOFCODE

---
+++ Q100/main.js
@@ -0,0 +1,6 @@
+// Question 100: Use the JavaScript Math object to find the square root of 144.
+// Calculate the square root of 144 using Math.sqrt()
+var number = 144;
+var squareRoot1 = Math.sqrt(number);
+console.log("The square root of ".concat(number, " is: ").concat(squareRoot1));
+// This line effectively calculates and shows the square root of 144.
---
+++ Q100/main.ts
@@ -0,0 +1,9 @@
+// Question 100: Use the JavaScript Math object to find the square root of 144.
+
+// Calculate the square root of 144 using Math.sqrt()
+const number = 144;
+const squareRoot1 = Math.sqrt(number);
+
+console.log(`The square root of ${number} is: ${squareRoot1}`);
+// This line effectively calculates and shows the square root of 144.
+

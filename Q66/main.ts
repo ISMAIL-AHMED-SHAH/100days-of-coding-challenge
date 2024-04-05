1e59908ec2cf5c935e2a7a9f921a503d6d460578
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-05 10:14:33 +0500

    100DAYSOFCODE

---
+++ Q66/main.ts
@@ -0,0 +1,18 @@
+// Question 66: Logical AND Verification: Create a function that checks two boolean (true or false) values. It should only say true if both are true, using the && operator. For instance, checkBothTrue(true, false) should be false.
+
+
+function checkBothTrue(val1: boolean, val2: boolean): boolean {
+    
+    return val1 && val2;
+}
+
+
+console.log(checkBothTrue(true, false));
+// It checks two things, but since one is false, the answer is false.
+
+console.log(("\n"));
+
+console.log(checkBothTrue(true, true));  // Output: True
+console.log(checkBothTrue(true, false)); // Output: False
+console.log(checkBothTrue(false, true)); // Output: False
+console.log(checkBothTrue(false, false)); // Output: False

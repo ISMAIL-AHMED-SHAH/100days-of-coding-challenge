58c07daf469044d958551d4a6206bfcf577992cd
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-07 08:41:49 +0500

    100DAYSOFCODE

---
+++ Q109/main.ts
@@ -0,0 +1,20 @@
+// Question 109: Write an if statement that logs "Good Morning" if the current time is before 12 PM.
+
+// Explain & TIP: You can get the current hour using the Date object and its getHours() method. Remember, hours are in 24-hour format, so 12 PM is 12.
+
+// Get the current date and time
+let currentDate = new Date();
+// Get the current hour (in 24-hour format)
+let currentHour = currentDate.getHours();
+
+// Check different time slots and print appropriate message
+if (currentHour < 12) {
+    console.log("Good Morning");
+} else if (currentHour < 18) {
+    console.log("Good Afternoon");
+} else if (currentHour) {
+    console.log("Good Evening");
+} else {
+    console.log("Good Night");
+}
+// This simple check helps us greet users appropriately based on the time of day.

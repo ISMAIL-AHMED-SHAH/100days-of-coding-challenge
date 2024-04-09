0c9d014c7f20ca2955f2d748682f99e03b13bd40
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-09 07:16:23 +0500

    100DAYSOFCODE

---
+++ Q117/app.ts
@@ -0,0 +1,27 @@
+// Question 117: Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.
+
+// Explain & TIP: The default case in a switch statement provides a way to execute a block of code when none of the other case labels match the expression's value.
+
+// function evaluateExpression(value: number): string {
+//     let result: string;
+
+//     switch (value) {
+//         case 1:
+//             result = "One";
+//             break;
+//         case 2:
+//             result = "Two";
+//             break;
+//         case 3:
+//             result = "Three";
+//             break;
+//         default:
+//             result = "Other";
+//     }
+
+//     return result;
+// }
+
+// // Example usage
+// console.log(evaluateExpression(2)); // Output: Two
+// console.log(evaluateExpression(5)); // Output: Other

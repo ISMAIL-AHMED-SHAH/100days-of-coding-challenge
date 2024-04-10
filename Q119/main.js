1663d33660917c04f93d1e27493bf0b251330137
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-11 00:33:07 +0500

    100DAYSOFCODE

---
+++ Q119/main.js
@@ -0,0 +1,9 @@
+// Question 119: Create a while loop that logs "Hello, World!" 5 times.
+// Initializes a counter
+var count = 0;
+// This while loop runs until count is 5
+while (count < 5) {
+    console.log("Hello, World!"); // Logs "Hello, World!"
+    count++; // Increments count by 1
+}
+// Repeats logging "Hello, World!" 5 times, once per loop iteration.

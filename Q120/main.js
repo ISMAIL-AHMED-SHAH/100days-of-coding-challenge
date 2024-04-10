d82d5de1c56e65ca260693dc1eaa30c259cad381
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-11 00:31:16 +0500

    100DAYSOFCODE

---
+++ Q120/main.js
@@ -0,0 +1,8 @@
+// Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+// Array of favorite movies
+var favoriteMovies = ["Inception", "The Dark Knight", "Interstellar", "The Shawshank Redemption", "Pulp Fiction"];
+// Use a for...of loop to iterate through the array and log each movie
+for (var _i = 0, favoriteMovies_1 = favoriteMovies; _i < favoriteMovies_1.length; _i++) {
+    var movie = favoriteMovies_1[_i];
+    console.log(movie);
+}
---
+++ Q120/main.ts
@@ -0,0 +1,9 @@
+// Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+// Array of favorite movies
+const favoriteMovies: string[] = ["Inception", "The Dark Knight", "Interstellar", "The Shawshank Redemption", "Pulp Fiction"];
+
+// Use a for...of loop to iterate through the array and log each movie
+for (const movie of favoriteMovies) {
+    console.log(movie);
+}

6cf77539786f0d69e44d9ab118f87e9a85f9f236
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-12 20:34:35 +0500

    100DAYSOFCODE

---
+++ Q50/main.js
@@ -0,0 +1,6 @@
+// Question 50: Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+// Explain & TIP: Template literals support multiline strings, making it easier to create readable text blocks that incorporate variables and expressions.
+// Using template literals to define a multiline string
+var myIdealDay = "My ideal day would involve:\n1. Waking up early and going for a jog.\n2. Spending a few hours coding on a personal project.\n3. Ending the day by reading a good book.";
+// Logging the multiline string to the console
+console.log(myIdealDay);
---
+++ Q50/main.ts
@@ -0,0 +1,12 @@
+// Question 50: Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+
+// Explain & TIP: Template literals support multiline strings, making it easier to create readable text blocks that incorporate variables and expressions.
+
+// Using template literals to define a multiline string
+let myIdealDay = `My ideal day would involve:
+1. Waking up early and going for a jog.
+2. Spending a few hours coding on a personal project.
+3. Ending the day by reading a good book.`;
+
+// Logging the multiline string to the console
+console.log(myIdealDay);

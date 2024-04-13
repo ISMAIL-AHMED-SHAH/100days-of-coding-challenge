df08e51da3c202268d9850489a2deba27a5e6356
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-13 06:10:58 +0500

    100DAYSOFCODE

---
+++ Q127/main.js
@@ -0,0 +1,11 @@
+// (Q127) Converting Traditional Functions to Arrow Functions
+// Explored converting traditional function expressions into arrow functions, observing the concise syntax and improved readability.
+// // Traditional function expression
+var traditionalFunction = function (a, b) {
+    return a + b;
+};
+// Converted to arrow function
+var arrowFunction = function (a, b) { return a + b; };
+console.log(traditionalFunction(5, 3)); // Outputs: 8
+console.log(arrowFunction(5, 3)); // Outputs: 8
+// Demonstrates the conversion of a traditional function expression to an arrow function.

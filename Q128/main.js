2c0dde17f2861270ab7443e1d555616992c5d767
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-13 06:16:54 +0500

    100DAYSOFCODE

---
+++ Q128/main.js
@@ -0,0 +1,12 @@
+// (Q128) Calculating the Product of Parameters
+// Implemented a function using rest parameters to calculate the product of any number of parameters, showcasing the flexibility it offers.
+// Arrow function that calculates the product of all its parameters
+var multiplyParameters = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (total, number) { return total * number; }, 1);
+};
+console.log(multiplyParameters(2, 3, 4)); // Outputs: 24
+// This function uses the rest parameter syntax to take any number of arguments, then multiplies them together.

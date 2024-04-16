79d704dcda299496a5e3cde0add55d15a6bfc8c2
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-16 06:49:02 +0500

    100DAYSOFCODE

---
+++ Q136/main.js
@@ -0,0 +1,9 @@
+// Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+// Demonstrates using console.log() inside a loop to track variable values
+for (var i = 0; i < 5; i++) {
+    // Variable whose value we want to track
+    var currentValue = i * 2;
+    // Output the value of the variable
+    console.log("Current value of currentValue:", currentValue);
+}
+// This loop iterates five times, logging the value of 'i' during each iteration to help with debugging.
---
+++ Q136/main.ts
@@ -0,0 +1,12 @@
+// Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+
+// using console.log() inside a loop to track variable values
+for (let i = 0; i < 5; i++) {
+    // Variable whose value we want to track
+    let currentValue = i * 2;
+
+    // Output the value of the variable
+    console.log("Current value of currentValue:", currentValue);
+
+}
+  // This loop iterates five times, logging the value of 'i' during each iteration to help with debugging.

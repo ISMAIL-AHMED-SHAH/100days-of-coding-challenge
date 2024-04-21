5ef1f66cf31969e59d8efad4b1971e6859969d1d
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-21 15:52:32 +0500

    100DAYSOFCODE

---
+++ Q150/app.js
@@ -0,0 +1,7 @@
+// Asynchronous Callbacks
+setTimeout(function () {
+    console.log("Delayed execution!");
+}, 2000); // Executes after 2000 milliseconds (2 seconds)
+// Synchronous Code Execution
+console.log("Start"); // Outputs: "Start" immediately
+console.log("Middle"); // Outputs:
---
+++ Q150/app.ts
@@ -0,0 +1,13 @@
+// Question 150: Describe how asynchronous callbacks differ from synchronous code execution.
+
+// Asynchronous Callbacks
+setTimeout(() => {
+    console.log("Delayed execution!");
+  }, 2000); // Executes after 2000 milliseconds (2 seconds)
+  
+  // Synchronous Code Execution
+  console.log("Start"); // Outputs: "Start" immediately
+  console.log("Middle"); // Outputs:
+  
+
+  

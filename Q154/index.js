371370ea67d85d122a8a6716ecba049008bc880a
ISMAIL-AHMED-SHAH <159659680+ISMAIL-AHMED-SHAH@users.noreply.github.com>
2024-04-24 04:07:34 +0500

    100DAYSOFCODE

---
+++ Q154/index.js
@@ -0,0 +1,29 @@
+let message;
+
+message = 'Hello World!';
+
+message = 'Hello Coderssss!';    // value changed
+
+alert(message);
+
+
+let admin, name; // can declare two variables at once
+
+name = "Ali";
+
+admin = name;
+
+alert( admin );
+
+
+let currentUserName = "The Current user Name is ISMAIL";
+
+alert( currentUserName );
+
+
+let ourPlanetName = "Our Planet Name is Earth";
+
+alert( ourPlanetName );
+
+
+
